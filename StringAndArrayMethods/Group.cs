bea7ff6fd3505cbcad974087fd9c84cb2ba877b4
Cavid112233 <suleymanli535@gmail.com>
2024-04-01 01:40:37 +0400

    studentsAndGroups

---
+++ StringAndArrayMethods/Group.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAndArrayMethods
+{
+    internal class Group
+    {
+        public string No { get; }
+        public List<Student> Students { get; }
+        public int StudentLimit { get; }
+
+        public Group(string no, int studentLimit)
+        {
+            No = no;
+            Students = new List<Student>();
+            StudentLimit = studentLimit;
+        }
+
+        public void AddStudent(Student student)
+        {
+            if (Students.Count < StudentLimit)
+            {
+                Students.Add(student);
+                Console.WriteLine($"{student.FullName} bu qrupa elave olundu.");
+            }
+            else
+            {
+                Console.WriteLine("Artig telebe daxil etmek olmur!");
+            }
+        }
+    }
+}
---
+++ StringAndArrayMethods/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAndArrayMethods
+{
+    internal class Student
+    {
+        public string FullName { get; set; }
+        public string GroupNo { get; set; }
+        public double AvgPoint { get; set; }
+    }
+}
